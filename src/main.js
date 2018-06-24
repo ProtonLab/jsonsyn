165799cbe3d260c781b60626003ace8ce48184d9
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-24 17:20:09 +0200

    Add getAllFiles function

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+
+/**
+ * Find all files inside a dir, recursively.
+ * @function getAllFiles
+ * @param  {string} dir Dir path string.
+ * @return {string[]} Array with all file names that are inside the directory.
+ */
+const getAllFiles = dir =>
+  fs.readdirSync(dir).reduce((files, file) => {
+    const name = path.join(dir, file);
+    const isDirectory = fs.statSync(name).isDirectory();
+    return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+  }, []);
